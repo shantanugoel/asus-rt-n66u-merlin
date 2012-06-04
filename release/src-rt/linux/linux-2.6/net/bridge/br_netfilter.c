4c84b57a3c3df66d1cf5df2e06ee4793153162f0
Eric Sauvageau <rmerl@lostrealm.ca>
2012-06-04 01:43:05 -0400

    Reverted to build 108 QoS code - new code requires the bridge netfilter module, which is unstable.

--- release/src-rt/linux/linux-2.6/net/bridge/br_netfilter.c
+++ release/src-rt/linux/linux-2.6/net/bridge/br_netfilter.c
@@ -55,11 +55,11 @@
 
 #ifdef CONFIG_SYSCTL
 static struct ctl_table_header *brnf_sysctl_header;
-static int brnf_call_iptables __read_mostly = 0;
-static int brnf_call_ip6tables __read_mostly = 0;
-static int brnf_call_arptables __read_mostly = 0;
-static int brnf_filter_vlan_tagged __read_mostly = 0;
-static int brnf_filter_pppoe_tagged __read_mostly = 0;
+static int brnf_call_iptables __read_mostly = 1;
+static int brnf_call_ip6tables __read_mostly = 1;
+static int brnf_call_arptables __read_mostly = 1;
+static int brnf_filter_vlan_tagged __read_mostly = 1;
+static int brnf_filter_pppoe_tagged __read_mostly = 1;
 #else
 #define brnf_filter_vlan_tagged 1
 #define brnf_filter_pppoe_tagged 1
